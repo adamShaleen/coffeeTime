f4d2d17433901e2215ab793fa66ef9892548a803
adamShaleen <adamshaleen@gmail.com>
2017-02-19 22:51:15 -0600

    basics

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("#button").click(function() {
+
+    // get current time
+    var timeStamp = new Date();
+    var hours = timeStamp.getHours();
+    var minutes = timeStamp.getMinutes();
+
+    // check to see if it's between 9:30-11:30 or 1:30-5
+    if (hours >= 9 && hours <= 11 && minutes <= 30 || hours >= 1 && hours <= 5 && minutes <= 30) {
+      $("#response").text("YES");
+    } else {
+      $("#response").text("NO");
+    }
+  });
+
+}); // Closing
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
-A cray basic web app because I couldn't remember the cortisol clock and didn't want to google every time.  Inspiration: http://neurosciencedc.blogspot.jp/2013/10/the-best-time-for-your-coffee.html
+A cray basic web app because I couldn't remember the cortisol clock
+and didn't want to google every time.
+Inspiration: http://neurosciencedc.blogspot.jp/2013/10/the-best-time-for-your-coffee.html
