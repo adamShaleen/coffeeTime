a606a0de2c0584b1e984e3bb2e0702ecc099b07c
adamShaleen <adamshaleen@gmail.com>
2017-02-25 09:17:02 -0600

    refactored main function and edited text.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,19 @@ $(document).ready(function() {
     var timeStamp = new Date();
     var hours = timeStamp.getHours();
     var minutes = timeStamp.getMinutes();
+    var amOrPm = "";
+
+    if (hours < 12) {
+      amOrPm = "AM";
+    } else {
+      amOrPm = "PM";
+    }
 
     // check to see if it's between 9:30-11:30 or 1:30-5
-    if (hours >= 9 && hours <= 11 && minutes <= 30 || hours >= 13 && hours <= 17 && minutes <= 30) {
-      $("#response").text("YES");
+    if ((hours >= 9 && minutes >= 30) && (hours <= 11 && minutes <= 30) || (hours >= 13 && minutes >= 30) && (hours <= 17 && minutes <= 30)) {
+      $("#response").text("YES, because it's " + hours + ":" + minutes + " " + amOrPm);
     } else {
-      $("#response").text("NO");
+      $("#response").text("NO, because it's " + hours + ":" + minutes + " " + amOrPm);
     }
   });
 
