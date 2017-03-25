c6c527d19e532f11c8bc7505c798e1ce56317634
adamShaleen <adamshaleen@gmail.com>
2017-03-24 20:37:44 -0500

    added jank logic for minutes less than 10

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,21 @@ $(document).ready(function() {
 
     // check to see if it's between 9:30-11:30 or 1:30-5
     if ((hours >= 9 && minutes >= 30) && (hours <= 11 && minutes <= 30) || (hours >= 13 && minutes >= 30) && (hours <= 17 && minutes <= 30)) {
-      $("#response").text("YES, because it's " + hours + ":" + minutes + " " + amOrPm);
+
+        // if minutes is less than 10 add the preceding zero
+        if (minutes < 10) {
+            $("#response").text("YES, because it's " + hours + ":0" + minutes + " " + amOrPm);
+        }
+
+        $("#response").text("YES, because it's " + hours + ":" + minutes + " " + amOrPm);
     } else {
-      $("#response").text("NO, because it's " + hours + ":" + minutes + " " + amOrPm);
+
+        if (minutes < 10) {
+            $("#response").text("NO, because it's " + hours + ":0" + minutes + " " + amOrPm);
+        }
+    $("#response").text("NO, because it's " + hours + ":" + minutes + " " + amOrPm);
     }
+
   });
 
 }); // Closing
